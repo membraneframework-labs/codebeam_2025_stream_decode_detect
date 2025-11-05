524c8acc434a17a2231d87188ca36a796682279e
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2025-11-05 14:17:29 +0100

    Enhance module documentation for HLS playlist setup
    Updated module documentation with detailed instructions for obtaining a valid HLS playlist address.

--- lib/demos/boombox_with_yolo/elixir.ex
+++ lib/demos/boombox_with_yolo/elixir.ex
@@ -1,10 +1,18 @@
 defmodule Demos.BoomboxWithYOLO.Elixir do
   @moduledoc """
   To run this demo, you have to
-   - make sure that HLS playlist address is valid
-   - download ONNX model and classes data so that paths used in
+    - make sure that HLS playlist address is valid
+    - download ONNX model and classes data so that paths used in
      `Demos.BoomboxWithYOLO.Elixir.GenServer.init/1` are correct
      (follow instructions from https://github.com/poeticoding/yolo_elixir/blob/main/examples/yolox.livemd#download-yolox-model)
+
+  In order to get a new HLS playslit address, you can 
+    - enter a webpage like [this](https://www.skylinewebcams.com/en/webcam/italia/liguria/imperia/festival-sanremo-ariston.html)
+    - inspect the webpage
+    - go to Network tab
+    - add "m3u8" filter
+    - and look for a playlist address. It should contain m3u8 extension, but it doesn't necessarily has to be at the end of the address. 
+
   """
 
   def demo() do
